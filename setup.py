fb5f71a947a376864323fefd80fafd548e14158d
lusCombeNCut <sv22482@bristol.ac.uk>
2024-06-11 22:54:26 +0100

    adjusted naming and formatting

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(
-  name='sentinel1decoder',
-  url='https://github.com/Rich-Hall/sentinel1decoder',
+  name='sentinel1decoderCustom',
+  url='https://github.com/lusCombeNCut/sentinel1decoderCustom.git',
   author='Rich Hall',
-  author_email='richardhall434@gmail.com',
-  packages=['sentinel1decoder',],
+  author_email='sv22482@bristol.ac.uk',
+  packages=['sentinel1decoderCustom',],
   install_requires=['numpy', 'pandas'],
   version='0.1',
   license='GPL-3.0',
